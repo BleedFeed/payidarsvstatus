f594f8250f7d626b7308b5ef15ed38ac01fe7c46
erenbaltali16 <erenbaltali16@gmail.com>
2023-01-22 22:39:08 +0300

    requestTimeout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const util = require('minecraft-server-util');
 const express = require('express');
 
 const options = {
-    timeout: 1000 * 15, 
+    timeout: 1000 * process.env.requestTimeout, 
     enableSRV: true 
 };
 
