3a634bfb711a5c0b052b8f998f4c473f6b610459
erenbaltali16 <erenbaltali16@gmail.com>
2023-01-26 13:21:13 +0300

    websocket

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
  
 const { Client } = require('discord.js-selfbot-v13');
 const util = require('minecraft-server-util');
+const {WebSocketServer} = require('ws');
+let websocketChannels = {};
 require('dotenv').config();
 
 const options = {
@@ -26,11 +28,54 @@ const checkForSpam = (message) => {
 
 client.on('ready', async () => {
         console.log(`${client.user.username} is ready!`);
+
+        let wss = new WebSocketServer({port:process.env.PORT});
+        wss.on('connection',(ws) =>{
+                let name;
+                let channel;
+                ws.on('error',console.error);
+
+                ws.on('message',async (data)=> {
+                        let message = dataToJSON(data);
+                        if(typeof message.type !== 'string'|| typeof message.message !== 'string') {return;}
+                        if(message.type === 'init' && typeof message.name === "string"){
+                                   
+                                name = message.name;
+                                channel = await client.guilds.cache.get('360902871933386753').channels.cache.get('1068073829068447784').createChannel(name);
+                                websocketChannels[channel.id] = ws;
+                                channel.send(`**${name}** >> ${message.message}`);
+                        }
+                        else if(message.type === 'message' && channel){
+                                console.log("asdsa");
+                                channel.send(`**${name}** >> ${message.message}`);
+                        }
+                });
+
+                ws.on('close',()=>{
+                        if(!channel.deleted){channel.delete()}
+                        delete websocketChannels[channel.id];
+                })
+        });
+
+
 })
+
+function dataToJSON(data){
+        try{
+                return JSON.parse(data.toString());
+        }
+        catch(err){
+                console.error("not a valid json");
+        }
+}
+
 
 client.on('message', async (message) => {
                 if(cooldownList.indexOf(message.author.id) !== -1){return true;}
-                if ((message.content === "sunucu açık mı?" || message.content === "Sunucu açık mı?"))
+                if(websocketChannels[message.channelId] && message.author.username !== "d4rkpr1nce"){
+                        websocketChannels[message.channelId].send(JSON.stringify({name: message.member.displayName,message:message.content}));
+                }
+                else if ((message.content === "sunucu açık mı?" || message.content === "Sunucu açık mı?"))
                 {
                         util.status('payidar.rabisu.net', 25845, options)
                             .then((result) => message.reply(`Sunucu açık ve şuan oynayan ${result.players.online} kişi var.`))
@@ -69,5 +114,7 @@ client.on('message', async (message) => {
                 }
 
 });
+
+
 
 client.login(process.env.token);
